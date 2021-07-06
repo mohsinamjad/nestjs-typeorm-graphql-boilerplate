02bde0a7f7cf4d8bb5a66d82bb2b4e83bd64ebbf
Mohsin <mohsin.amjad@intechww.com>
2021-07-06 22:14:03 +0500

    added config module

--- apps/scratch/src/app.module.ts
+++ apps/scratch/src/app.module.ts
@@ -1,22 +1,27 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { getConnectionOptions } from 'typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-// import { TypeGraphQLModule } from 'typegraphql-nestjs';
-import { AppResolver } from './app.resolver';
-import { CommonModule } from '@libs/common';
-import { LoggerMiddleware } from '@libs/common';
 import { AuthModule } from '@libs/auth';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 import { CustomThrottlerGuard } from '@libs/auth/guards/throttler-guard';
 import { SeedModule } from '@libs/auth/seed/seed.module';
+import { CommonModule, LoggerMiddleware } from '@libs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+import { typeOrmConfigAsync } from './config/ormconfig';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: false,
+      envFilePath: `apps/scratch/env/.env.${process.env.NODE_ENV || 'dev'}`,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     GraphQLModule.forRoot({
       /**
        * it will auto generate schema.gql from objectTypes/Entities
@@ -32,18 +37,6 @@ import { SeedModule } from '@libs/auth/seed/seed.module';
      * Once this is done, the TypeORM Connection and EntityManager objects will be available to inject
      * across the entire project (without needing to import any modules)
      */
-    TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        Object.assign(await getConnectionOptions(), {
-          /**
-           * Manually adding entities to the entities array of the connection options can be tedious
-           * With that option specified, every entity registered through the forFeature() method will
-           * be automatically added to the entities array of the configuration object.
-           * here we are merging ormconfig with extra @nest/typeorm configs
-           */
-          autoLoadEntities: true,
-        }),
-    }),
     ThrottlerModule.forRoot({
       ttl: 10,
       limit: 5,
