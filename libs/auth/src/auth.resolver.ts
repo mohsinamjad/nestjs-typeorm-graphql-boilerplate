992e97de36aef2dd687e3f3dee6300a14cc97a1d
Mohsin <mohsin.amjad@intechww.com>
2021-07-15 15:16:36 +0500

    updated local auth guard

--- libs/auth/src/auth.resolver.ts
+++ libs/auth/src/auth.resolver.ts
@@ -1,23 +1,21 @@
-import { Injectable, Request } from '@nestjs/common';
-import { Args, Mutation } from '@nestjs/graphql';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth-guard';
 import { loginInput } from './resources/user/dto/user.dto';
-import { LocalStrategy } from './passport-strategies/local.strategy';
 
 @Injectable()
 export class AuthResolver {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly localStrategy: LocalStrategy,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Mutation(() => String)
   async login(
-    @Request() req,
-    @Args('data') { email, password }: loginInput,
+    @Context() ctx,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    @Args('data') loginCreds: loginInput,
   ): Promise<string> {
-    const user = await this.localStrategy.validate(email, password);
-    const response = await this.authService.login(user);
+    const response = await this.authService.login(ctx?.req?.user);
     return response.access_token;
   }
 }
--- libs/auth/src/guards/local-auth-guard.ts
+++ libs/auth/src/guards/local-auth-guard.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 // local is default strategy name defined in passport
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  // Override this method so it can be used in graphql
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const gqlReq = ctx.getContext().req;
+    if (gqlReq) {
+      const { data } = ctx.getArgs();
+      gqlReq.body = data;
+      return gqlReq;
+    }
+    return context.switchToHttp().getRequest();
+  }
+}
